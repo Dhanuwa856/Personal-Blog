9cd338fc8f1057f2dc7c3d698cf7c2e6a6c61360
Dhanushka Rathnayaka <pdhanushka856@gmail.com>
2024-05-28 21:58:18 +0530

    test

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -2,7 +2,16 @@ import React from "react";
 import "./About.css";
 
 const About = () => {
-  return <div></div>;
+  return (
+    <div className="about container">
+      <p className="p-text">
+        Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex
+        aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos
+        quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat
+        sit in iste officiis commodi quidem hic quas.
+      </p>
+    </div>
+  );
 };
 
 export default About;
